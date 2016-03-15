5930b4dac47765adad4f7b6df2181e472de5107d
Blake Scott and Ryan Brown <blakerscott@gmail.com>
2016-03-15 10:12:20 -0700

    add descriptive comments to the gulpfile tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,20 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
+var jshint = require('gulp-jshint');
+
+//Here is the bit of code we will use to tell which kind of environment we are using
 var buildProduction = utilities.env.production;
 
+//Concatenates all files with the "-interface.js" ending to a filename and puts it into the tmp file
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
 
+//Running concatInterface first, as a dependency, then browserifying.
+//concatInterface is needed as a dependency of this task because it is always necessary.
 gulp.task('jsBrowserify', ['concatInterface'] , function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
@@ -20,16 +26,20 @@ gulp.task('jsBrowserify', ['concatInterface'] , function() {
     .pipe(gulp.dest('./build/js'));
 });
 
+//Minifies, or uglifies, the scripts in app.js for faster browser performance.  jsBrowserify is a dependency, therefore concatInterface
+// will also be run.
 gulp.task("minifyScripts", ["jsBrowserify"], function(){
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
     .pipe(gulp.dest("./build/js"));
 });
 
+//Deletes existing build and tmp files. This is a dependency of the build task that is below.
 gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
 
+//Creates either a dev or production build.  This is determined by the absence or presence of the --production flag. If it is a production build, it starts uglify, else is starts browserify.
 gulp.task("build", ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
@@ -38,14 +48,7 @@ gulp.task("build", ['clean'], function(){
   }
 });
 
-gulp.task("build", function(){
-  if (buildProduction) {
-    gulp.start('minifyScripts');
-  } else {
-    gulp.start('jsBrowserify');
-  }
-});
-
+//Allows you to debug your JS code by running "gulp jshint" in the terminal
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
