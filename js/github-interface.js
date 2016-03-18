586b5edc75db347b08bc7540047b483021e2a26f
Blake Scott <blakerscott@gmail.com>
2016-03-18 12:32:54 -0700

    add for loop that appends repo names and description to the display

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,21 +1,17 @@
 var userInput = require('./../js/userInput.js').userInput;
 
   exports.getRepos = function(){
-    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    $.get('https://api.github.com/users/' + gitName + '/repos?access_token=' + apiKey).then(function(response){
       console.log(response);
+
+      for(var i = 0; i < response.length; i++)
+      {
+        $('.showRepos').append('<ul><li> Repo Name: ' + response[i].name + '</li></ul>');
+         $('.showRepos').append('<ul><li> Description: ' + response[i].description + '</li></ul>');
+      }
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
   };
 
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=5&stolen_after=1451610062', function(response) {
-      console.log(response);
-      for(var i = 0; i < response.bikes.length; i++) {
-        $('.showRepos').append(response.bikes[i].title);
-        $('.showRepos').append('<ul><li>'+ response.bikes[i].stolen_location + '</li></ul>');
-        $('#address').val("'" + response.bikes[i].stolen_location + "'");
-        Address();
-      }
-    });
-  });
-});
+  
