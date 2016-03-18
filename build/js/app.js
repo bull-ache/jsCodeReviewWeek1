1293a9efefb0f069bd311d6249bbbd432262e402
Blake Scott <blakerscott@gmail.com>
2016-03-18 15:26:40 -0700

    add functionality to remove the contents of a previous search when a user resubmits

--- build/js/app.js
+++ build/js/app.js
@@ -25,6 +25,8 @@ var getRepos = require('./../js/github-interface.js').getRepos;
 $(document).ready(function(){
   $('#getName').submit(function(event){
   event.preventDefault();
+  $('#picture').empty("");
+  $('.showRepos').empty("");
   var repos = $('#name').val();
   getRepos(repos);
   $('#name').val("");
