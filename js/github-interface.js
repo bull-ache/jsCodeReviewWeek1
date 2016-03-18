71f8a9ddb235646a0df026422351bd4297c27426
Blake Scott <blakerscott@gmail.com>
2016-03-18 12:09:28 -0700

    change var and input names on js and index files

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,31 +1,18 @@
 var userInput = require('./../js/userInput.js').userInput;
 
+  exports.getRepos = function(){
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  };
 
-
-
-
-
-
-
-    exports.getRepos = function(){
-      $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-        console.log(response);
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
-    };
-
-
-
-
-
-
-
-      $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=5&stolen_after=1451610062', function(response) {
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=5&stolen_after=1451610062', function(response) {
       console.log(response);
       for(var i = 0; i < response.bikes.length; i++) {
-        $('.showBikes').append(response.bikes[i].title);
-        $('.showBikes').append('<ul><li>'+ response.bikes[i].stolen_location + '</li></ul>');
+        $('.showRepos').append(response.bikes[i].title);
+        $('.showRepos').append('<ul><li>'+ response.bikes[i].stolen_location + '</li></ul>');
         $('#address').val("'" + response.bikes[i].stolen_location + "'");
         Address();
       }
--- js/userInput.js
+++ js/userInput.js
@@ -1,3 +1,10 @@
-$('#repos').click(function() {
-  var repos = $('#location').val();
-  $('#location').val("");
+var getRepo = require('./../js/github-request.js').getRepo;
+
+$(document).ready(function(){
+  $('#search').click(function(event){
+  event.preventDefault();
+  var repos = $('#name').val();
+  getRepos(repos);
+  $('#name').val("");
+  });
+});
