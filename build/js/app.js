27d3a99e8163cab12915ce2ced4dceaf3dfac312
Blake Scott <blakerscott@gmail.com>
2016-03-18 12:52:44 -0700

    remove css link tag from head of document

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "3ec084b296ca7bdfdc9169ce1ddde7106c2878d1";
 
+},{}],2:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
 
-},{}]},{},[1]);
+  exports.getRepos = function(repos){
+    $.get('https://api.github.com/users/' + gitName + '/repos?access_token=' + apiKey).then(function(response){
+      console.log(response);
+
+      for(var i = 0; i < response.length; i++)
+      {
+        $('.showRepos').append('<ul><li> Repo Name: ' + response[i].name + '</li></ul>');
+         $('.showRepos').append('<ul><li> Description: ' + response[i].description + '</li></ul>');
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  };
+
+},{"./../.env":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,1 +1,15 @@
+var apiKey = require('./../.env').apiKey;
 
+  exports.getRepos = function(repos){
+    $.get('https://api.github.com/users/' + gitName + '/repos?access_token=' + apiKey).then(function(response){
+      console.log(response);
+
+      for(var i = 0; i < response.length; i++)
+      {
+        $('.showRepos').append('<ul><li> Repo Name: ' + response[i].name + '</li></ul>');
+         $('.showRepos').append('<ul><li> Description: ' + response[i].description + '</li></ul>');
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  };
