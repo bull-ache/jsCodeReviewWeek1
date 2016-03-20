4bd82a7b84e6ce7dd3529a8fc61cfb456d7951f6
Blake Scott <blakerscott@gmail.com>
2016-03-20 16:57:34 -0700

    add basic template to getProfile

---
+++ js/getProfile.js
@@ -0,0 +1,16 @@
+var apiKey = require('./../.env').apiKey;
+
+  exports.getRepos = function(repos){
+    $.get('https://api.github.com/users/' + repos + '/repos?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      $('#picture').append('<img src='+ response[0].owner.avatar_url +'>');
+
+      for(var i = 0; i < response.length; i++)
+      {
+        $('.showRepos').append('<ul><li> Repo Name: ' + response[i].full_name + '</li></ul>');
+         $('.showRepos').append('<ul><li> Description: ' + response[i].description + '</li></ul>');
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  };
