07bd233f44ff1a8b46e0701b30f755de613050fc
Blake Scott <blakerscott@gmail.com>
2016-03-20 22:01:45 -0700

    add additional colors to scss file

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -16,6 +16,8 @@ $(document).ready(function(){
   });
   $('#getName').submit(function(event){
   event.preventDefault();
+  $('#picture').empty("");
+  $('.showRepos').empty("");
   var repos = $('#name').val();
   getRepos(repos);
   $('#name').val("");
