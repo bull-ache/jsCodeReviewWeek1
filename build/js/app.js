7de81916f5ce3d7ab4a5ca86cc2479a40a89cfab
Blake Scott <blakerscott@gmail.com>
2016-03-21 09:36:01 -0700

    update code by removing array brackets from getProfile show lines so that the profile search will work

--- build/js/app.js
+++ build/js/app.js
@@ -7,11 +7,11 @@ var apiKey = require('./../.env').apiKey;
   exports.getProfile = function(repos){
     $.get('https://api.github.com/users/' + repos + '?access_token=' + apiKey).then(function(response){
       console.log(response);
-      $('#showPicture').append('<img class="img-circle" src='+ response[0].avatar_url +'>');
-      $('#showFullName').append('<ul><li> Real Name: ' + response[0].name + '</li></ul>');
-      $('#showEmail').append('<ul><li> Description: ' + response[0].email + '</li></ul>');
-      $('#showFollowers').append('<ul><li> Repo Name: ' + response[0].followers + '</li></ul>');
-      $('#showFollowing').append('<ul><li> Description: ' + response[0].following + '</li></ul>');
+      $('#showPicture').append('<img class="img-circle" src='+ response.avatar_url +'>');
+      $('#showFullName').append('<ul><li> Real Name: ' + response.name + '</li></ul>');
+      $('#showEmail').append('<ul><li> Description: ' + response.email + '</li></ul>');
+      $('#showFollowers').append('<ul><li> Followers: ' + response.followers + '</li></ul>');
+      $('#showFollowing').append('<ul><li> Following: ' + response.following + '</li></ul>');
     })
     .fail(function(error){
       console.log(error.responseJSON.message);
